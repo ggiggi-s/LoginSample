814faade62d5e2b32150bb4bcd1b82ca0cb80b5d
Leethyun <leethw@naver.com>
2019-03-02 15:39:30 +0900

    Init a Project

---
+++ test_api_server/src/main/java/com/hyun/test_api_server/TestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.hyun.test_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiServerApplication.class, args);
+	}
+
+}
---
+++ test_api_server/src/test/java/com/hyun/test_api_server/TestApiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hyun.test_api_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
